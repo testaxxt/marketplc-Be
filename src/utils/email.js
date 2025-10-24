623e8d10c834955d0e4ab68ca4d4a4c368c5ad59
streaktv8@gmail.com <streaktv8@gmail.com>
2025-10-25 00:18:33 +0100

    added config

--- src/utils/email.js
+++ src/utils/email.js
@@ -1,4 +1,8 @@
 const nodemailer = require('nodemailer');
+const dotenv = require("dotenv")
+
+
+dotenv.config()
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,   // e.g. smtp.gmail.com
